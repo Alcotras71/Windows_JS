1ba7f3ac776223eec369d47627f7d24001bb45b7
Andrey <AimDragMan2@yandex.ru>
2020-09-24 23:10:24 +0300

    addImagesModule

---
+++ src/js/modules/images.js
@@ -0,0 +1,36 @@
+const images = () => {
+	const imgPopup = document.createElement('div'),
+			workSection = document.querySelector('.works'),
+			bigImage = document.createElement('img');
+
+	imgPopup.classList.add('popup');
+	workSection.appendChild(imgPopup);
+
+	imgPopup.style.justifyContent = 'center';
+	imgPopup.style.alignItems = 'center';
+	imgPopup.style.display = 'none';
+	bigImage.style.maxWidth = '600px';
+	bigImage.style.maxHeight = '500px';
+
+	imgPopup.appendChild(bigImage);
+
+	workSection.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		let target = e.target;
+
+		if ( target && target.classList.contains('preview')) {
+			imgPopup.style.display = 'flex';
+			document.body.classList.add('modal-open');
+			const path = target.parentNode.getAttribute('href');
+			bigImage.setAttribute('src', path);
+		}
+
+		if (target && target.matches('div.popup')) {
+			imgPopup.style.display = 'none';
+			document.body.classList.remove('modal-open');
+		}
+	});
+};
+
+export default images;
